64fa11748dfdb13960a55530a11670a17a177f32
Soudehm <satkinu@gmail.com>
2017-01-09 16:44:02 -0800

    Update index.php

--- _includes/index.php
+++ _includes/index.php
@@ -4,14 +4,13 @@
       <p>E-mail:<p> <input type="email" name="email"> <br />
       <p>Comment: <br><textarea name="comment" rows="5" cols="40"></textarea>
   <br>
-  <input type="radio" name="information" value="generalinquiries" checked> General inquiries<br>
-  <input type="radio" name="information" value="requestanappointment"> Request an appointment<br>
-  <input type="radio" name="information" value="other"> Other <br>
+  <input type="radio" name="urgency" value="generalinquiries" checked> General inquiries<br>
+  <input type="radio" name="urgency" value="requestanappointment"> Request an appointment<br>
+  <input type="radio" name="urgency" value="other"> Other <br>
   <input type="submit" name="submit" value="Send">  
   <br><br>
        <input type="hidden" name="_next" value="//www.juniorcareer.com/thanks.md"/>
-       <input type="text" name="_gotcha" style="display:none"/>
-</form>        
+        
    <script>
          var contactform = document.getElementById('formation');
          contactform.setAttribute('action','//formspree.io/'+'info'+'@'+'juniorcareer'+'.'+'com');
